6d1685e7ccb14dfa195c27f4a7f851ba98d7b601
Still Hsu <5843208+Still34@users.noreply.github.com>
2019-09-12 10:17:14 +0800

    Revise Chapter 3 exercise 	* Make the exercise a little more responsive and understandable to the user

--- exercise/ch3/app.js
+++ exercise/ch3/app.js
@@ -1,22 +1,28 @@
 let express = require('express');
 let app = express();
-
-let purify = require('./validation/purify');
-let responseHeaderConfig = require('./configs/responseHeaderConfig')
-
-responseHeaderConfig(app);
+let port = 3000;
+// Remedy for sanitization
+//      let purify = require('./validation/purify');
+// Sets up CSP
+//      let responseHeaderConfig = require('./configs/responseHeaderConfig')
+//      responseHeaderConfig(app);
 
 app.get('/', (req, res) => {
     // Instant XSS
     // http://localhost:3000/?input=%3Cscript%3Ealert(1)%3C/script%3E
     let input = req.query.input;
-    // Unsanitized string
-    res.send(input);
-    // Sanitized string
-    // res.send(purify.sanitize(input));
+    if (input) {
+        // Unsanitized string
+        let output = `You sent me '${input}'.`;
+        // Sanitized string
+        // let output = `You sent me ${purify.sanitize(input)}.`;
+        res.send(output);
+    } else {
+        res.send(`Send me something with <a href='${req.protocol}://${req.hostname}:${port}/?input=Hello+World!'>this link</a>!`);
+    }
 });
 
-app.listen(3000, function () {
+app.listen(port, function () {
     let path = require('path');
     let currentDir = path.basename(path.dirname(require.main.filename));
     console.log(`[${currentDir}] Begin listening on port 3000.`);
